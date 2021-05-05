05590d3a9bc1568ab89fc5468207d7ae8c238d05
Joshua Wright <jawright91@gmail.com>
2021-05-04 19:11:27 -0500

    added routes for products and individual product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def one_product
+    product = Product.first
+    render json: product
+  end
+
+  def all_products
+    products = Product.all
+    render json: products
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/product", controller: "products", action: "one_product"
+  get "/products", controller: "products", action: "all_products"
 end
