c94044190b147de83406b3df07f542e32d6df2a9
Joshua Wright <jawright91@gmail.com>
2021-05-05 20:41:43 -0500

    added ability to view multiple individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,18 @@
 class ProductsController < ApplicationController
   def one_product
-    product = Product.first
-    render json: product
+    if params["id"] == nil
+      product = "error: no product entered"
+    else
+      product = Product.find_by(id: params["id"])
+      if product == nil
+        product = "Invalid ID"
+      end
+    end
+    render json: product.as_json
   end
 
   def all_products
     products = Product.all
-    render json: products
+    render json: products.as_json
   end
 end
