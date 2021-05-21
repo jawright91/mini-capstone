bba46dd395c88ef89df939e840df9a30d755de1f
Joshua Wright <jawright91@gmail.com>
2021-05-20 20:15:01 -0500

    added has many/belongs to relationships for shopping cart functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,12 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { in: 10..500 }
 
-  has_many :orders
+  has_many :orders, through: :carted_products
   belongs_to :supplier
   has_many :images
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
 
   def is_discounted?
     return price <= 10
